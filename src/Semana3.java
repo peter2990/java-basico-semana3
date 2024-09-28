5d79a1e29b890c21afedf33c05baf08a24c55841
peter2990 <ritmoysonlatino507@gmail.com>
2024-09-28 14:52:12 -0500

    Ya se escribio el codigo relacionado con los comentario y tipos de datos.

---
+++ src/Semana3.java
@@ -0,0 +1,24 @@
+public class Semana3 {
+    public static void main(String[] args) {
+        //cualquier cosa
+
+        /* cualquier cosa
+           y otras cosas  */
+
+        short numeroEntero = 1;
+        byte numeroEntero2 =5;
+        int numeroEntero3 =10; //usen esto
+        long numeroEntero4 =100;
+
+        float numerodecimal =3.14f;
+        double numerodecimal2 =3.14; // usen este
+
+        char letras = 'A';
+        String texto = "hablen, pelaos";
+
+        boolean esVerdadero = true;
+
+
+
+    }
+}
